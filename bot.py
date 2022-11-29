8db44b5fd1c378b105506ed503c3592a41d3abd3
sunsword2020 <73904317+sunsword2020@users.noreply.github.com>
2022-11-29 16:11:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ logging.getLogger().setLevel(logging.WARNING)
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from utils import Media
+from aiohttp import web
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, PORT
 
 
